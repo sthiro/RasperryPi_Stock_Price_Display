c8dc9d78bbc999be0ccb52222582840f90cac780
*sthiro* <sthiroshaan@gmail.com>
2023-07-04 14:10:30 +0530

    A

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
-import drivers
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-# Load the driver and set it to "display"
-# If you use something from the driver library use the "display." prefix first
-display = drivers.Lcd()
+def event_callback(pin):
+    value = GPIO.input(pin)
+    print(f"pin :: {pin}, value is {value}")
 
-# Main body of code
-try:
-    while True:
-        # Remember that your sentences can only be 16 characters long!
-        print("Writing to display")
-        display.lcd_display_string("Greetings Human!", 1)  # Write line of text to first line of display
-        display.lcd_display_string("Demo Pi Guy code", 2)  # Write line of text to second line of display
-        sleep(2)                                           # Give time for the message to be read
-        display.lcd_display_string("I am a display!", 1)   # Refresh the first line of display with a different message
-        sleep(2)                                           # Give time for the message to be read
-        display.lcd_clear()                                # Clear the display of any data
-        sleep(2)                                           # Give time for the message to be read
-except KeyboardInterrupt:
-    # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
-    print("Cleaning up!")
-    display.lcd_clear()
+if __name__ == '__main__':
+    button_pin = 23
+    row_pin = 17
 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(row_pin, GPIO.OUT)
+    GPIO.setup(button_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+    GPIO.output(row_pin, GPIO.LOW)
+
+    # events can be GPIO.RISING, GPIO.FALLING, or GPIO.BOTH
+    GPIO.add_event_detect(button_pin, GPIO.BOTH,
+                          callback=event_callback,
+                          bouncetime=300)
+
+    try:
+        time.sleep(1000)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
